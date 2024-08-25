883c7c8a3d664d0355f6779733c9cc50f1d8dd43
Phil Girard <philippe.girard5@gmail.com>
2024-08-25 16:53:19 -0400

    udpate bin

---
+++ bin/update-lampjs.sh
@@ -0,0 +1,3 @@
+git fetch git@github.com:philippegirard/LAMP-JS.git master && git merge FETCH_HEAD --allow-unrelated-histories
+# Using rebase instead of merge
+# git fetch git@github.com:philippegirard/LAMP-JS.git master && git rebase FETCH_HEAD --allow-unrelated-histories
